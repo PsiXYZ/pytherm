6663e6421cee4050ad22d330e9153aaa7f46a688
Ignaty Efimov <>
2022-10-13 16:22:54 +0300

    update PSRK

--- pytherm/eos/psrk.py
+++ pytherm/eos/psrk.py
@@ -1,6 +1,7 @@
-from math import log, sqrt
+from math import exp, log, sqrt
 from pytherm import constants
 from pytherm.activity.activity_model import Activity_model
+import numpy as np
 
 R = constants.R
 
@@ -77,10 +78,7 @@ class PSRK:
         return b
 
     def get_ge(self, system, T):
-        if len(system) == 1:
-            return 0
-        else:
-            return self.activity_model.get_ge(system, T)
+        return self.activity_model.get_ge(system, T)
 
     def get_cubic_coef(self, system, T, p):
         alphas = self.get_alphas(T)
@@ -95,3 +93,47 @@ class PSRK:
                 - R * T / p,
                 a / p - b * R * T / p - b ** 2,
                 - a * b / p)
+
+    def get_roots(self, system, P, T):
+        cs = self.get_cubic_coef(system=system, T=T, p=P)
+        return (min(np.roots(cs)), max(np.roots(cs)))
+
+    def get_f(self, system, P, V, T):
+        alphas = self.get_alphas(T)
+        ai = self.get_ai(alphas)
+        bi = self.get_bi()
+
+        alp = {}
+        for i in system:
+            alp[i] = ai[i] / (bi[i] * R * T)
+
+        b = self.get_b(system, bi)
+
+        yi = self.activity_model.get_y(system, T)
+        # s = 0
+        # for i in system:
+        #     s += system[i] * log(yi[i])
+        # ge = R * T * s
+
+        # a = self.get_a(system, T, b, ai, bi, ge)
+        # roots = self.get_roots(system=system, P=P, T=T)
+        der_ai = self.get_der_ai(yi, b, bi, alp)
+
+        def f(v):
+            lnf = {}
+            for i in system:
+                lnf[i] = bi[i] / b * ((P * v) / (R * T) - 1)
+                - log(P * (v - b) / (R * T))
+                - der_ai[i] * log((v + b) / v)
+            fi = {}
+            for i in lnf:
+                fi[i] = exp(lnf[i])
+            return fi
+        return f(V)
+
+    def get_der_ai(self, y, b, bi, alp, A=-0.64663):
+        der_ai = {}
+        for i in y:
+            der_ai[i] = 1 / A * (log(y[i]) + log(b / bi[i]) +
+                                 bi[i] / b - 1) + alp[i]
+        return der_ai
