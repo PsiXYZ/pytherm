ff2743b2fe5c5fd2ede1b1db308d3d939cd42ff1
Ignaty Efimov <>
2022-10-18 11:48:35 +0300

    add abstract methods to eos.py

--- pytherm/eos/eos.py
+++ pytherm/eos/eos.py
@@ -1,3 +1,15 @@
-class EOS:
+from abc import ABC, abstractmethod
+
+
+class EOS(ABC):
+    @abstractmethod
     def get_system(self):
+        pass
+
+    @abstractmethod
+    def get_f(self):
+        pass
+
+    @abstractmethod
+    def get_p(self):
         pass
