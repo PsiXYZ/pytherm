38fd938b38af55f3f10914dff2887947a9d9186b
Ignaty <efimov.ignaty@gmail.com>
2023-04-30 20:23:43 +0300

    remove unifac_old.py

--- setup.py
+++ setup.py
@@ -2,24 +2,24 @@ from setuptools import setup, find_packages
 import pytherm
 
 with open("README.rst", "r") as readme_file:
-  readme = readme_file.read()
+    readme = readme_file.read()
 
-requirements = ['numpy',]
+requirements = ['numpy', ]
 
 setup(
-  name = 'pytherm',
-  # packages = ['pytherm'],
-  packages=find_packages(),
-  license='MIT',
-  version = pytherm.__version__,
-  description = 'Pytherm is an open-source scientific Python package for thermodynamic modeling.',
-  author = 'Ignaty Efimov',
-  install_requires=requirements,
-  long_description=readme,
-  author_email = 'efimov.ignaty@gmail.com',
-  url = 'https://github.com/PsiXYZ/pytherm',
-  python_requires='>=3.9',
-  # package_data={'pytherm': ['activity/db/*',]},
-  include_package_data=True,
+    name='pytherm',
+    # packages = ['pytherm'],
+    packages=find_packages(),
+    license='MIT',
+    version=pytherm.__version__,
+    description='Pytherm is an open-source scientific Python package for thermodynamic modeling.',
+    author='Ignaty Efimov',
+    install_requires=requirements,
+    long_description=readme,
+    author_email='efimov.ignaty@gmail.com',
+    url='https://github.com/PsiXYZ/pytherm',
+    python_requires='>=3.9',
+    # package_data={'pytherm': ['activity/db/*',]},
+    include_package_data=True,
 
 )
